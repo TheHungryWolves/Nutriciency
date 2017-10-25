e08dcde8e4456d92cbd56c42eb0f03b6ba766fc4
jjorgensenjr <33075127+jjorgensenjr@users.noreply.github.com>
2017-10-25 01:51:28 -0400

    Add files via upload

---
+++ FoodBank.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class FoodBank
+{
+    private String name;
+    private int phoneNumber;
+    private int officeNumber;
+    private String street;
+    private String city;
+    private String state;
+    private int zipcode;
+    private String country;
+    private String lastUpdatedBy;
+    private DateTime lastUpdated;
+    private bool isDeleted;
+
+    public FoodBank(String name, int phoneNumber, int officeNumber, String street, String city, String state, int zipcode,
+        String country, String lastUpdatedBy, DateTime lastUpdated, bool isDeleted)
+    {
+        setName(name);
+        setPhoneNumber(phoneNumber);
+        setOfficeNumber(officeNumber);
+        setStreet(street);
+        setCity(city);
+        setState(state);
+        setZip(zipcode);
+        setCountry(country);
+        setLastUpdatedBy(lastUpdatedBy);
+        setLastUpdated(lastUpdated);
+        setIsDeleted(isDeleted);
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public void setPhoneNumber(int phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getPhoneNumber()
+    {
+        return this.phoneNumber;
+    }
+
+    public void setOfficeNumber(int officeNumber)
+    {
+        this.officeNumber = officeNumber;
+    }
+
+    public int getOfficeNumber()
+    {
+        return this.officeNumber;
+    }
+
+    public void setStreet(String street)
+    {
+        this.street = street;
+    }
+
+    public String getStreet()
+    {
+        return this.street;
+    }
+
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
+
+    public String getCity()
+    {
+        return this.city;
+    }
+
+    public void setState(String state)
+    {
+        this.state = state;
+    }
+
+    public String getState()
+    {
+        return this.state;
+    }
+
+    public void setZip(int zipcode)
+    {
+        this.zipcode = zipcode;
+    }
+
+    public int getZip()
+    {
+        return this.zipcode;
+    }
+
+    public void setCountry(String country)
+    {
+        this.country = country;
+    }
+
+    public String getCountry()
+    {
+        return this.country;
+    }
+
+    public void setLastUpdatedBy(String lastUpdatedBy)
+    {
+        this.lastUpdatedBy = lastUpdatedBy;
+    }
+
+    public String getLastUpdatedBy()
+    {
+        return this.lastUpdatedBy;
+    }
+
+    public void setLastUpdated(DateTime lastUpdated)
+    {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public DateTime getlastUpdated()
+    {
+        return this.lastUpdated;
+    }
+
+    public void setIsDeleted(bool isDeleted)
+    {
+        this.isDeleted = isDeleted;
+    }
+
+    public bool getIsDeleted()
+    {
+        return this.isDeleted;
+    }
+}
