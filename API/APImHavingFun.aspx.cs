74bbfa4543c921a8034e9bf3cdd0aabe010ec587
jjorgensenjr <33075127+jjorgensenjr@users.noreply.github.com>
2017-10-25 13:28:55 -0400

    Add files via upload

---
+++ API/APImHavingFun.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class APImHavingFun : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Unnamed2_Click(object sender, EventArgs e)
+    {
+        Talking client = new Talking();
+        client.endPoint = txt1.Text;
+
+        string response = client.makeRequest();
+
+        Label1.Text = response;
+    }
+}
